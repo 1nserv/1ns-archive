52247734d4826b563b1865c007f9975ba01190e6
happex <110610727+okayhappex@users.noreply.github.com>
2024-06-22 23:00:57 +0200

    Introduction des votes

--- nsarchive/cls/votes.py
+++ nsarchive/cls/votes.py
@@ -1,7 +1,16 @@
 import time
 
+from .entities import FunctionalUser
+
 class Vote:
-    def __init__(self, id: int, title: str, choices: list[str]) -> None:
-        self.id: int = id
+    def __init__(self, id: str, title: str, choices: tuple[str]) -> None:
+        self.id: str = id
         self.title: str = title
         self.choices = { choice : 0 for choice in choices }
+        self.author: FunctionalUser
+        self.startDate: int = 0
+        self.endDate: int = 0
+
+class ClosedVote(Vote):
+    def __init__(self, id: str, title: str) -> None:
+        super().__init__(id, title, ('yes', 'no', 'blank'))
